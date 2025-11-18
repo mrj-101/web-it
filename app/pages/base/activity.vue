957d18bb6b6604b287162daddff649c7244839c1
Surachai Visesvoharn <mrj@rvc.ac.th>
2025-11-18 12:58:48 +0700

    update activity

--- app/pages/base/activity.vue
+++ app/pages/base/activity.vue
@@ -1,11 +1,11 @@
 <script setup>
-const { error, pending, data } = useFetch('http://www.rvc.ac.th:9000/api/activity?status=1')
-console.log(data.value)
+const data = await $fetch("http://www.rvc.ac.th:9000/api/activity?status=1")
+// const { error, pending, data } = await useFetch('http://www.rvc.ac.th:9000/api/activity?status=1')
 </script>
 
 <template>
   <div>
-    <h1>Activity</h1>
-
+    <h1 class="text-2xl text-blue-600 mb-8">กิจกรรมภายในวิทยาลัย</h1>
+    {{ data }}
   </div>
 </template>
